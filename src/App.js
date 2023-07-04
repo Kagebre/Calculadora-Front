84e8557b1f90c127623e9bda8ede2cf0f124e7d5
Benhur <benhur.custodio@gmail.com>
2023-07-04 08:33:27 -0300

    tabela implementada

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import api from './api.js';
 import './App.css';
 
-
 function App() {
   const [resultado, setResultado] = useState('');
   const [numerosDigitados, setNumerosDigitados] = useState('');
-  const [ultimaOperacao, setUltimaOperacao] = useState('');
+  const [ultimaOperacao, setUltimaOperacao] = useState([]);
 
-  async function getOperacoes(){
-    const chamaOp = await api.get ('/consultar_operacoes');
-    console.log(chamaOp.data)
-
+  async function getOperacoes() {
+    try {
+      const response = await api.get('/consultar_operacoes');
+      const operacoes = response.data;
+      console.log(operacoes);
+      setUltimaOperacao(operacoes);
+    } catch (error) {
+      console.error('Erro ao consultar operações:', error);
+    }
   }
 
   async function getCalculo() {
@@ -21,7 +25,7 @@ function App() {
       });
       console.log('resultCalc', resultCalc);
       setResultado(resultCalc.data);
-      setUltimaOperacao(numerosDigitados);
+      setUltimaOperacao([...ultimaOperacao, { operacao: numerosDigitados, resultado: resultCalc.data, data_hora: new Date().toLocaleString() }]);
     } catch (error) {
       console.error(error);
     }
@@ -63,7 +67,7 @@ function App() {
   function apagatudo() {
     setNumerosDigitados('');
     setResultado('');
-    setUltimaOperacao('');
+    
   }
 
   function apagarUltimo() {
@@ -71,54 +75,75 @@ function App() {
   }
 
   return (
-    <div className="App" >
-      <h1 className="calculator-title">CALCULADORA DAS TREVAS</h1>
-     
+    <div className="App">
+      <h1 className="calculator-title">CALCULADORA LAZARENTA</h1>
+
       <div className="result-window">{resultado}</div>
 
       <p>Números Digitados: {numerosDigitados}</p>
-      <div> 
-        <button className="highlight calculator-button" onClick={apagatudo}>C</button>
 
-        <button name="M" className="calculator-button">M</button>
-        <button name="CE" onClick={apagarUltimo}className="calculator-button">←</button>
-        <button name="H"  onClick={getOperacoes}className="calculator-button">H</button>
-
-        </div>   
       <div>
-          <button name="7" onClick={handleClick}className="calculator-button">7</button>
-          <button name="8" onClick={handleClick}className="calculator-button">8</button>
-          <button name="9" onClick={handleClick}className="calculator-button">9</button>
-          <button name="/" onClick={handleClick}className="calculator-button">/</button>
-          <button name="**.5" onClick={handleClick}className="calculator-button">√</button>
-        </div>
-        <div>
-          <button name="4" onClick={handleClick}className="calculator-button">4</button>
-          <button name="5" onClick={handleClick}className="calculator-button">5</button>
-          <button name="6" onClick={handleClick}className="calculator-button">6</button>
-          <button name="*" onClick={handleClick}className="calculator-button">*</button>
-          <button name="**2" onClick={handleClick}className="calculator-button">x²</button> 
-        </div>
-        <div>
-          <button name="1" onClick={handleClick}className="calculator-button">1</button>
-          <button name="2" onClick={handleClick}className="calculator-button">2</button>
-          <button name="3" onClick={handleClick}className="calculator-button">3</button>
-          <button name="-" onClick={handleClick}className="calculator-button">-</button>
-          <button name="(" onClick={handleClick}className="calculator-button">(</button>
-        </div>
-
-        <button name="." onClick={handleClick}className="calculator-button">.</button>
-        <button name="0" onClick={handleClick}className="calculator-button">0</button>
-        <button name="+" onClick={handleClick}className="calculator-button">+</button>
-        <button className="highlight equal-button" onClick={getCalculo}>=</button>
-        <button name=")" onClick={handleClick}className="calculator-button">)</button>
-        <div>        
-
-          <div id="operacoesContainer">
-            Última Operação: {ultimaOperacao}
-          </div>
-        </div>
+        <button className="highlight calculator-button" onClick={apagatudo}>C</button>
+        <button name="M" className="calculator-button">M</button>
+        <button name="CE" onClick={apagarUltimo} className="calculator-button">←</button>
+        <button name="H" onClick={getOperacoes} className="calculator-button">H</button>
       </div>
+
+      <div>
+        <button name="7" onClick={handleClick} className="calculator-button">7</button>
+        <button name="8" onClick={handleClick} className="calculator-button">8</button>
+        <button name="9" onClick={handleClick} className="calculator-button">9</button>
+        <button name="/" onClick={handleClick} className="calculator-button">/</button>
+        <button name="**.5" onClick={handleClick} className="calculator-button">√</button>
+      </div>
+
+      <div>
+        <button name="4" onClick={handleClick} className="calculator-button">4</button>
+        <button name="5" onClick={handleClick} className="calculator-button">5</button>
+        <button name="6" onClick={handleClick} className="calculator-button">6</button>
+        <button name="*" onClick={handleClick} className="calculator-button">*</button>
+        <button name="**2" onClick={handleClick} className="calculator-button">x²</button>
+      </div>
+
+      <div>
+        <button name="1" onClick={handleClick} className="calculator-button">1</button>
+        <button name="2" onClick={handleClick} className="calculator-button">2</button>
+        <button name="3" onClick={handleClick} className="calculator-button">3</button>
+        <button name="-" onClick={handleClick} className="calculator-button">-</button>
+        <button name="(" onClick={handleClick} className="calculator-button">(</button>
+      </div>
+
+      <button name="." onClick={handleClick} className="calculator-button">.</button>
+      <button name="0" onClick={handleClick} className="calculator-button">0</button>
+      <button name="+" onClick={handleClick} className="calculator-button">+</button>
+      <button className="highlight equal-button" onClick={getCalculo}>=</button>
+      <button name=")" onClick={handleClick} className="calculator-button">)</button>
+
+      <div className="App">
+        <div id="operacoesContainer">
+          Última Operação:
+        </div>
+
+        <table className="tabela-operacoes">
+          <thead>
+            <tr>
+              <th>Operação</th>
+              <th>Resultado</th>
+              <th>Data e Hora</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ultimaOperacao.map((operacao, index) => (
+              <tr key={index}>
+                <td>{operacao.operacao}</td>
+                <td>{operacao.resultado}</td>
+                <td>{operacao.data_hora}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
